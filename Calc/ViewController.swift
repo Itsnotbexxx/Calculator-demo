080023fb5b5e857052038bb297354f7694a9eed9
Itsnotbexxx <nurpeis.beka@gmail.com>
2021-11-28 16:20:44 +0600

    first commit

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var myDisplay: UILabel!
+    
+    var typing: Bool = false
+    @IBAction func digitPressed(_ sender: UIButton) {
+        let current_digit = sender.currentTitle!
+        if typing{
+        let current_display = myDisplay.text!
+        myDisplay.text = current_display + current_digit
+        }else{
+            myDisplay.text = current_digit
+            typing = true
+        }
     }
-
-
+    var displayValue: Double{
+        get{
+            return Double(myDisplay.text!)!
+        }
+        set{
+            myDisplay.text = String(newValue)
+        }
+    }
+    private var calculatorModel = CalculatorModel()
+    @IBAction func operationPressed(_ sender: UIButton) {
+        calculatorModel.setOperand(displayValue)
+        calculatorModel.performOperand(sender.currentTitle!)
+        displayValue = calculatorModel.getResult()!
+        
+        typing = false
+    }
 }
 
--- Calc/calcModel.swift
+++ Calc/calcModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+
+func makeZero(opernd: Double) -> Double{
+    return 0
+}
+
+enum Operation{
+    case constant(Double),unaryOperation((Double)->Double),binaryOperation((Double,Double)->Double),equals
+}
+
+
+struct CalculatorModel {
+    var my_operation: Dictionary<String,Operation> =
+        [
+            "p": Operation.constant(Double.pi),
+            "e": Operation.constant(M_E),
+            "√": Operation.unaryOperation(sqrt),
+            "sin": Operation.unaryOperation(sin),
+            "A/C": Operation.unaryOperation(makeZero),
+            "+": Operation.binaryOperation({$0+$1}), 
+            "-": Operation.binaryOperation({$0-$1}),
+            "×": Operation.binaryOperation({$0*$1}),
+            "÷": Operation.binaryOperation({$0/$1}),
+            "=": Operation.equals
+        ]
+    
+    private var global_value: Double?
+    mutating func setOperand(_ operand: Double){
+        global_value = operand
+    }
+    mutating func performOperand(_ operation: String){
+        let symbol = my_operation[operation]!
+        switch symbol{
+        case .constant(let value):
+            global_value = value
+        case .unaryOperation(let function):
+            global_value = function(global_value!)
+        case .binaryOperation(let function):
+            saving = SaveFirstOperandAndOperation(firstOperand: global_value!, operation: function)
+        case .equals:
+            global_value = saving?.performOperationWith(secondOperand: global_value!)
+        }
+        
+    }
+    func getResult()-> Double?{
+        return global_value
+    }
+    private var saving: SaveFirstOperandAndOperation?
+    struct SaveFirstOperandAndOperation {
+        var firstOperand: Double
+        var operation: (Double,Double) -> Double
+        
+        func performOperationWith(secondOperand op2:Double) -> Double{
+            return operation(firstOperand,op2)
+        }
+    }
+}
